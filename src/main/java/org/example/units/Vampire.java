949f251a93e9aa70065c7fae3d4fef06846a5cdd
maks <gluamsk21@gmail.com>
2022-10-19 12:09:26 +0300

    Refactor

--- src/main/java/org/example/units/Vampire.java
+++ src/main/java/org/example/units/Vampire.java
@@ -3,15 +3,15 @@ package org.example.units;
 public class Vampire extends Warrior {
     public static final int STANDARD_HEALTH = 40;
     public static final int STANDARD_ATTACK = 4;
-    public static final double STANDARD_REGEN_PERCENT = 0.50;
+    public static final int STANDARD_REGEN_PERCENT = 50;
 
-    private double regenPercent;
+    private int regenPercent;
 
     public Vampire() {
         this(STANDARD_HEALTH, STANDARD_ATTACK, STANDARD_REGEN_PERCENT);
     }
 
-    public Vampire(int health, int attack, double regenPercent) {
+    public Vampire(int health, int attack, int regenPercent) {
         super(health, attack);
         if (regenPercent < 0) {
             throw new IllegalArgumentException();
@@ -24,7 +24,7 @@ public class Vampire extends Warrior {
         return regenPercent;
     }
 
-    public void setRegen(double regenPercent) {
+    public void setRegen(int regenPercent) {
         this.regenPercent = Math.max(0, regenPercent);
     }
 
@@ -33,7 +33,7 @@ public class Vampire extends Warrior {
         super.hit(opponent);
         int after = opponent.getHealth();
 
-        int regened = (int) ((before - after) * regenPercent);
-        heal(regened);
+        int regenerated = (before - after) * regenPercent / 100;
+        heal(regenerated);
     }
 }
